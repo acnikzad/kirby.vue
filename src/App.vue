e09078e6f85de5aa03681c5d430f6c20027062b5
“Aria <a.c.nikzad@gmail.com>
2020-10-06 09:42:23 -0700

    added bootstrap

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
 <!-- Navigation-->
     <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
         <div class="container">
-            <a class="navbar-brand js-scroll-trigger" href="#page-top">Kirby Trailers</a>
+            <a class="navbar-brand js-scroll-trigger" href="#page-top">4x4 Teardrop Trailers</a>
             <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
             <div class="collapse navbar-collapse" id="navbarResponsive">
                 <ul class="navbar-nav ml-auto my-2 my-lg-0">
--- src/views/Quote.vue
+++ src/views/Quote.vue
@@ -1,42 +1,37 @@
 <template>
   <section>
-    <div class="quote">
-      <h1>Build a Quote</h1>
-    </div>
-    <div class="row">
-      <div class="col span-1-of-2 options">
-        <p>Select trailer:</p>
-        <p>Select color:</p>
+    <h1 class="text-center">Build A Trailer</h1>
+    <form class="quote">
+      <div class="form-group">
+        <label for="firstname">First Name</label>
+        <input type="firstname" class="form-control" id="firstname" aria-describedby="emailHelp" placeholder="First Name">
       </div>
-      <form>
-        <div class="col span-1-of-2">
-          <select id="">
-            <option value="">--Please choose an option--</option>
-            <option value="XTR">XTR Off Road Teardrop Trailer</option>
-            <option value="XTR-XL">XTR-XL Off Road Teardrop Trailer</option>
-            <option value="XTC">XTC Off Road Teardrop Trailer</option>
-            <option value="ST-Sport">Xsport ST Teardrop Trailer</option>
-            <option value="XTS">XTS Model Teardrop Trailer</option>
-            <option value="Cub">Cub Classic Vintage Teardrop Trailer</option>
-            <option value="Camp">Command Camp Off Road Teardrop Trailer</option>
-            <option value="Blackhawk">Blackhawk Off Road Adventure Trailer</option>
-          </select>
-          <br>
-          <br>
-          <select id="">
-            <option value="">--Please choose an option--</option>
-            <option value="red">Red</option>
-            <option value="black">Black</option>
-            <option value="gray">Gray</option>
-            <option value="blue">Blue</option>
-            <option value="white">White</option>
-          </select>
-          <br>
-          <br>
-          <textarea name=”ContactCommentt” rows=”8″ cols=”50″>Questions or comments?</textarea>
-        </div>
-      </form>
-    </div>
+      <div class="form-group">
+        <label for="lastname">Last Name</label>
+        <input type="lastname" class="form-control" id="lastname" aria-describedby="emailHelp" placeholder="Last Name">
+      </div>
+      <div class="form-group">
+        <label for="phonenumber">Phone Number</label>
+        <input type="email" class="form-control" id="phonenumber" aria-describedby="emailHelp" placeholder="Phone Number">
+        <small id="emailHelp" class="form-text text-muted">We'll never share your phone number with anyone else.</small>
+      </div>
+      <div class="form-group">
+        <label for="exampleInputEmail1">Email address</label>
+        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+      </div>
+      <div class="form-group">
+        <label for="exampleFormControlSelect1">Select Trailer</label>
+        <select class="form-control" id="exampleFormControlSelect1">
+          <option>-- Select Trailer --</option>
+          <option>XTR Off Road</option>
+          <option>XTR-XL Off Road</option>
+          <option>XTC Off Road</option>
+          <option>Command Camp Off Road</option>
+        </select>
+      </div>
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
   </section>
 </template>
 
